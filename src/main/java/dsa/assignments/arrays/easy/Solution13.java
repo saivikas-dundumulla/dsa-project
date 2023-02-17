7842c82e8d3ef90752bdda675230eaac85229879
Sai Vikas Reddy Dundumulla <sdd54qr@admin.colruyt.int>
2023-02-17 18:09:01 +0100

    Solution 13 added.revisit it.

---
+++ src/main/java/dsa/assignments/arrays/easy/Solution13.java
@@ -0,0 +1,32 @@
+package dsa.assignments.arrays.easy;
+
+import java.util.Arrays;
+    /*      832. Flipping an Image
+            link:https://leetcode.com/problems/flipping-an-image/description/
+            Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+            To flip an image horizontally means that each row of the image is reversed.
+            For example, flipping [1,1,0] horizontally results in [0,1,1].
+            To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+            For example, inverting [0,1,1] results in [1,0,0].
+     */
+public class Solution13 {
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        for(int i=0;i<image.length;i++)
+            System.out.println(Arrays.toString(flipAndInvertImage(image)[i]));
+    }
+    public static int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++){
+            int start=0;
+            int end=image[i].length-1;
+            while(start<=end){
+                int temp=image[i][start];
+                image[i][start]=1-image[i][end];
+                image[i][end]=1-temp;
+                start++;
+                end--;
+            }
+        }
+        return image;
+    }
+}
